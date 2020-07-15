f55e07083e3ac0b4f0251f83cf7041d2e5c45d7a
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2020-07-15 09:35:16 -0300

    Done 1018

---
+++ 1018/index.js
@@ -0,0 +1,32 @@
+let valorDaNota = parseInt(prompt());
+
+let troco = valorDaNota;
+
+let cem = parseInt(troco / 100);
+troco = troco - cem * 100;
+
+let cinquenta = parseInt(troco / 50);
+troco = troco - cinquenta * 50;
+
+let vinte = parseInt(troco / 20);
+troco = troco - vinte * 20;
+
+let dez = parseInt(troco / 10);
+troco = troco - dez * 10;
+
+let cinco = parseInt(troco / 5);
+troco = troco - cinco * 5;
+
+let dois = parseInt(troco / 2);
+troco = troco - dois * 2;
+
+let um = parseInt(troco);
+
+console.log(valorDaNota);
+console.log(`${cem} nota(s) de R$ 100,00`);
+console.log(`${cinquenta} nota(s) de R$ 50,00`);
+console.log(`${vinte} nota(s) de R$ 20,00`);
+console.log(`${dez} nota(s) de R$ 10,00`);
+console.log(`${cinco} nota(s) de R$ 5,00`);
+console.log(`${dois} nota(s) de R$ 2,00`);
+console.log(`${um} nota(s) de R$ 1,00`);
